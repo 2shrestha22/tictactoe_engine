72337140bf65472425ed0d2e33a189113c4d3357
Sangam Shrestha <2shrestha22@gmail.com>
2022-10-04 18:15:49 +0545

    add testcase for wincombo check

--- packages/tictactoe_board/test/src/board_test.dart
+++ packages/tictactoe_board/test/src/board_test.dart
@@ -1,25 +1,50 @@
-import 'dart:developer';
-
 import 'package:test/test.dart';
 import 'package:tictactoe_board/src/mark.dart';
 import 'package:tictactoe_board/tictactoe_board.dart';
 
 void main() {
   late Board board;
+
   setUp(() {
     board = Board(3, 3, 3);
   });
 
   test(
-    'Board is workign properly..',
+    'Column check for win is working.',
     () async {
+      board.onComplete((Combo combo) {
+        expect(
+          combo,
+          equals([
+            Box(mark: Mark.zero, i: 0, j: 1),
+            Box(mark: Mark.zero, i: 1, j: 1),
+            Box(mark: Mark.zero, i: 2, j: 1),
+          ]),
+        );
+      });
+
       for (var i = 0; i < 3; i++) {
-        final combo = board.set(Mark.zero, i, 1);
-        if (combo != null) {
-          log(combo.toString());
-        }
+        board.set(Mark.zero, i, 1);
       }
-      log(board.toString());
+    },
+  );
+  test(
+    'Row check for win is working.',
+    () async {
+      board.onComplete((Combo combo) {
+        expect(
+          combo,
+          equals([
+            Box(mark: Mark.zero, i: 1, j: 0),
+            Box(mark: Mark.zero, i: 1, j: 1),
+            Box(mark: Mark.zero, i: 1, j: 2),
+          ]),
+        );
+      });
+
+      for (var i = 0; i < 3; i++) {
+        board.set(Mark.zero, 1, i);
+      }
     },
   );
 }
