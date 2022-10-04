70e7a26a7996b5e5a91e937685df1730f7df7adf
Sangam Shrestha <2shrestha22@gmail.com>
2022-10-04 21:25:53 +0545

    update tests

--- packages/tictactoe_board/test/src/board_test.dart
+++ packages/tictactoe_board/test/src/board_test.dart
@@ -4,47 +4,66 @@ import 'package:tictactoe_board/tictactoe_board.dart';
 
 void main() {
   late Board board;
+  Combo? combo;
 
   setUp(() {
+    combo = null;
     board = Board(3, 3, 3);
+    board.onComplete((Combo value) {
+      combo = value;
+    });
   });
 
   test(
     'Column check for win is working.',
     () async {
-      board.onComplete((Combo combo) {
-        expect(
-          combo,
-          equals([
-            Box(mark: Mark.zero, i: 0, j: 1),
-            Box(mark: Mark.zero, i: 1, j: 1),
-            Box(mark: Mark.zero, i: 2, j: 1),
-          ]),
-        );
-      });
-
       for (var i = 0; i < 3; i++) {
         board.set(Mark.zero, i, 1);
       }
+      expect(
+        combo,
+        equals([
+          Box(mark: Mark.zero, i: 0, j: 1),
+          Box(mark: Mark.zero, i: 1, j: 1),
+          Box(mark: Mark.zero, i: 2, j: 1),
+        ]),
+      );
     },
   );
   test(
     'Row check for win is working.',
     () async {
-      board.onComplete((Combo combo) {
-        expect(
-          combo,
-          equals([
-            Box(mark: Mark.zero, i: 1, j: 0),
-            Box(mark: Mark.zero, i: 1, j: 1),
-            Box(mark: Mark.zero, i: 1, j: 2),
-          ]),
-        );
-      });
-
       for (var i = 0; i < 3; i++) {
         board.set(Mark.zero, 1, i);
       }
+      expect(
+        combo,
+        equals([
+          Box(mark: Mark.zero, i: 1, j: 0),
+          Box(mark: Mark.zero, i: 1, j: 1),
+          Box(mark: Mark.zero, i: 1, j: 2),
+        ]),
+      );
+    },
+  );
+
+  test(
+    'Check diagonals for winCombo - /',
+    () async {
+      board.set(Mark.zero, 0, 0);
+      board.set(Mark.cross, 1, 2);
+      board.set(Mark.zero, 2, 2);
+      board.set(Mark.cross, 2, 1);
+      board.set(Mark.zero, 1, 1);
+
+      expect(
+        combo,
+        equals([
+          Box(mark: Mark.zero, i: 0, j: 0),
+          Box(mark: Mark.zero, i: 1, j: 1),
+          Box(mark: Mark.zero, i: 2, j: 2),
+        ]),
+      );
     },
   );
 }
